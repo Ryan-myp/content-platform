8a8b7beac2142b7c9e162d36fbbf8ff4004b9014
Ryan-myp <112227822+Ryan-myp@users.noreply.github.com>
2026-08-15 14:33:40 +0800

    chore: 移除测试辅助文件 serve_dist.mjs

--- serve_dist.mjs
+++
@@ -1,2 +0,0 @@
-import { startFrontend } from './deploy-cli/lib/server.js'
-await startFrontend('/Users/yanping.ma/PycharmProjects/Code-Platform/content-platform/frontend/dist', 3900, 'http://127.0.0.1:8893')
